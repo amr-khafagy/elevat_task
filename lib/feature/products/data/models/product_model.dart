1a63e056a85cfafcb696b4623203de4395c98ff5
amr-khafagy <amra0939@gmail.com>
2026-02-09 21:07:14 +0200

    add product model

--- lib/feature/products/data/models/product_model.dart
+++ lib/feature/products/data/models/product_model.dart
@@ -1,44 +1,26 @@
 class ProductModel {
-  int? id;
-  String? title;
-  double? price;
-  String? description;
-  String? category;
-  String? image;
-  Rating? rating;
+  final int id;
+  final String title;
+  final double price;
+  final String image;
+  final Rating? rating;
 
   ProductModel({
-    this.id,
-    this.title,
-    this.price,
-    this.description,
-    this.category,
-    this.image,
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.image,
     this.rating,
   });
 
-  ProductModel.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    title = json['title'];
-    price = json['price'];
-    description = json['description'];
-    category = json['category'];
-    image = json['image'];
-    rating = json['rating'] != null ? Rating.fromJson(json['rating']) : null;
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = <String, dynamic>{};
-    data['id'] = id;
-    data['title'] = title;
-    data['price'] = price;
-    data['description'] = description;
-    data['category'] = category;
-    data['image'] = image;
-    if (rating != null) {
-      data['rating'] = rating!.toJson();
-    }
-    return data;
+  factory ProductModel.fromJson(Map<String, dynamic> json) {
+    return ProductModel(
+      id: json['id'] as int,
+      title: json['title'] as String,
+      price: (json['price'] as num).toDouble(),
+      image: json['image'] as String,
+      rating: json['rating'] != null ? Rating.fromJson(json['rating']) : null,
+    );
   }
 }
 
